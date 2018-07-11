9d333747c7939858a40c7f5fdbc2d832c63cee36
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-07-11 10:54:16 +0200

    fixing import form reac-router-dom add div in router

--- lezarus/src/App.js
+++ lezarus/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Router, Link, Route} from 'react-router-dom'
-import {Intro} from '.Intro'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import Intro from './Intro'
+
 
 class App extends Component {
   render() {
@@ -11,12 +12,14 @@ class App extends Component {
               <h1>Lezarus</h1>
               <p>Szybki przewodnik oraz omówienie podstawowych funkcjonalności udostępnianych graczom.</p>
           </header>
+
         <Router>
+<div>
+
+            <Route exact path="/intro" component={Intro} />
             <Link to="/intro">Czym jest Lezarus?</Link>
 
-            <Route exact path="/intro" Component={Intro} />
-
-
+</div>
         </Router>
       </div>
     );

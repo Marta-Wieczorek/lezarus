b64b9458b099d1f9d70e5eace19ed73cdb3d03a0
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-07-11 10:45:22 +0200

    react-router-dom creating route to intro creating component Intro

--- lezarus/src/App.js
+++ lezarus/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Router, Link, Route} from 'react-router-dom'
+import {Intro} from '.Intro'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <header className="App-header">
+              <h1>Lezarus</h1>
+              <p>Szybki przewodnik oraz omówienie podstawowych funkcjonalności udostępnianych graczom.</p>
+          </header>
+        <Router>
+            <Link to="/intro">Czym jest Lezarus?</Link>
+
+            <Route exact path="/intro" Component={Intro} />
+
+
+        </Router>
       </div>
     );
   }
---
+++ lezarus/src/Intro.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Intro extends Component {
+    render () {
+        return (
+            <p>Tu będzie informacja o tym, czym jest Lezarus</p>
+        )
+    }
+
+}
+
+export default Intro;

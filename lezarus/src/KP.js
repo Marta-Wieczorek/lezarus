111f493bc1e0e1affbafcaa2b8732e8ae3e83bbc
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-07-23 15:26:29 +0200

    making metryka, placeholder for awek

--- lezarus/src/KP.js
+++ lezarus/src/KP.js
@@ -4,15 +4,19 @@ class KP extends Component {
     render() {
         return (
             <div className="kp">
-                <p>Imię:</p>
-                <p>Rasa:</p>
-                <p>Szczep:</p>
-                <p>Płeć:</p>
-                <p>Wiek:</p>
-                <p>Lokacja:</p>
-                <p>FPD:</p>
+                <div className="awek">Miejsce na awatar</div>
+                <div className="metryka">
+                    <p>Imię:</p>
+                    <p>Rasa:</p>
+                    <p>Szczep:</p>
+                    <p>Płeć:</p>
+                    <p>Wiek:</p>
+                    <p>Lokacja:</p>
+                    <p>FPD:</p>
+                </div>
             </div>
         )
     }
 }
+
 export default KP

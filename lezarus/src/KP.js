955a3d7d1813dca21a19fb813a03cb7a07283e5a
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-07-23 16:18:21 +0200

    metryka - formating

--- lezarus/src/KP.js
+++ lezarus/src/KP.js
@@ -6,13 +6,20 @@ class KP extends Component {
             <div className="kp">
                 <div className="awek">Miejsce na awatar</div>
                 <div className="metryka">
-                    <p>Imię:</p>
-                    <p>Rasa:</p>
-                    <p>Szczep:</p>
-                    <p>Płeć:</p>
-                    <p>Wiek:</p>
-                    <p>Lokacja:</p>
-                    <p>FPD:</p>
+                    <p className="metryka_wpis"><span>Imię:</span> <span>imię-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>Rasa:</span> <span>rasa-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>Szczep:</span> <span>szczep-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>Płeć:</span> <span>płeć-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>Wiek:</span> <span>wiek-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>Lokacja:</span> <span>lokacja-z-propsów</span></p>
+                    <hr />
+                    <p className="metryka_wpis"><span>FPD:</span> <span>FPD-z-propsow</span></p>
+                    <hr />
                 </div>
             </div>
         )

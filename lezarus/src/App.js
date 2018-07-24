3f7db7ad9dbd91fca59e1f90b3988a1865ea6b58
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-07-24 17:39:36 +0200

    fixing paddings

--- lezarus/src/App.js
+++ lezarus/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import Intro from './Intro'
@@ -7,26 +7,26 @@ import KP from "./KP";
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Lezarus</h1>
-              <p>Szybki przewodnik oraz omówienie podstawowych funkcjonalności udostępnianych graczom.</p>
-          </header>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Lezarus</h1>
+                    <p>Szybki przewodnik oraz omówienie podstawowych funkcjonalności udostępnianych graczom.</p>
+                </header>
 
-        <Router className="Router">
-<div className="right">
-    <Navbar/>
-    <Route exact path="/intro" component={Intro} />
-    <Route exact path="/kp" component={KP} />
+                <Router>
+                    <div>
+                        <Navbar/>
+                        <Route exact path="/intro" component={Intro}/>
+                        <Route exact path="/kp" component={KP}/>
 
 
-</div>
-        </Router>
-      </div>
-    );
-  }
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
